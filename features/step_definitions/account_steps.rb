cd70d0e5b805534e842ff1da87bab9f93eaa2d33
Christos Ioannidis <christos.ioan.gr@gmail.com>
2019-12-10 00:35:57 +0200

    Remove amount from account when money is withdrawn

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,4 +1,7 @@
-Given("I have deposited ${int} in my account") do |amount|
-  my_account.deposit(amount)
-  expect(my_account.balance).to eq(amount)
+Given("my account has been credited with ${int}") do |amount|
+  my_account.credit(amount)
+end
+
+Then("the balance of my account should be ${int}") do |amount|
+  expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
